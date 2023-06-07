a273b0e03df80b23f4c5eb9bb5dc716238c04b7c
Jcheol <neng_e@naver.com>
2023-06-07 17:34:15 +0900

    2023-06-07 Jcheol 구글 Oauth 리다이렉트 url로 앱으로 데이터 전송 성공 android:scheme="app", callbackUrlScheme: 'app'

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -149,7 +150,7 @@ Future<void> signInKakao() async {
 
   // 앱에서 받은 인증 코드를 백엔드로 전송하고 토큰을 받아올 수 있습니다.
   // 백엔드와의 통신 방식에 따라 구현하셔야 합니다.
-  sendAuthorizationCodeToBackend(code!);
+  //sendAuthorizationCodeToBackend(code!);
   // . . .
   // 액세스 토큰 저장 및 관리 등 추가 작업을 수행합니다.
   // . . .
@@ -158,11 +159,11 @@ Future<void> signInKakao() async {
 Future<void> signInGoogle() async {
   // 구글 로그인에 필요한 파라미터 설정
   final clientId = '900804701090-sk6rt9ah5cp1tmg6ppudj48ki2hs29co.apps.googleusercontent.com';
-  final redirectUri = 'http://api-respal.me/oauth/login/google';
+  final redirectUri = 'http://api-respal.me/oauth/app/login/google';
   final scopes = ['email', 'profile'];
 
   // 구글 로그인 URL
-  final loginUrl = 'https://accounts.google.com/o/oauth2/auth?client_id=900804701090-sk6rt9ah5cp1tmg6ppudj48ki2hs29co.apps.googleusercontent.com&redirect_uri=http://api-respal.me/oauth/login/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
+  final loginUrl = 'https://accounts.google.com/o/oauth2/auth?client_id=900804701090-sk6rt9ah5cp1tmg6ppudj48ki2hs29co.apps.googleusercontent.com&redirect_uri=http://api-respal.me/oauth/app/login/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
   // 로그인 요청 URL 생성
   final authUrl = Uri.parse(loginUrl).replace(queryParameters: {
     'client_id': clientId,
@@ -174,12 +175,29 @@ Future<void> signInGoogle() async {
   // 웹뷰를 통해 구글 로그인 페이지 열기
   final result = await FlutterWebAuth.authenticate(
     url: authUrl.toString(),
-    callbackUrlScheme: redirectUri,
+    callbackUrlScheme: 'app',
   );
-
+  var logger = Logger();
   // 콜백 데이터 처리
   final uri = Uri.parse(result);
-  final code = uri.queryParameters['code'];
+  final prefix = 'app://';
+  final path = result.substring(prefix.length);
+
+  String type = "";
+  if (path.startsWith('signup')) {
+    logger.d("신규회원");
+    type = "signup";
+  }else if(path.startsWith('callback')){
+    logger.d("기존회원");
+    type = "callback";
+  }else {
+    logger.d("에러");
+  }
+
+  final code = uri.queryParameters['uid'];
+  logger.d(code);
+
+  sendAuthorizationCodeToBackend(type, code!);
 
   // 앱에서 받은 인증 코드를 백엔드로 전송하고 토큰을 받아올 수 있습니다.
   // 백엔드와의 통신 방식에 따라 구현하셔야 합니다.
@@ -224,33 +242,39 @@ Future<void> signInGithub() async {
   // . . .
 }
 
-Future<void> sendAuthorizationCodeToBackend(String code) async {
+Future<void> sendAuthorizationCodeToBackend(String type, String code) async {
   final dio = Dio(); // Dio 인스턴스 생성
-
   try {
-    final response = await dio.post(
-      'http://3.36.185.63/member/login', // 백엔드 API 엔드포인트 URL
-      data: {
-        'code': code, // 인증 코드 전송
-      },
-    );
+    dio.get('http://api-respal.me/oauth/user/uid=' + code + '?type='+type);
+    var logger = Logger();
+    logger.d(dio.toString());
+  }catch(e){
+    print('Exception occurred: $e');
+  }
+
+    // final response = await dio.post(
+    //   'http://api-respal.me/oauth/user/'+code, // 백엔드 API 엔드포인트 URL
+    //   data: {
+    //     'code': code, // 인증 코드 전송
+    //   },
+    // );
 
     // 응답 처리
-    if (response.statusCode == 200) {
-      // 토큰을 성공적으로 받아온 경우
-      final token = response.data;
-      // 받아온 토큰을 앱에서 저장하거나 관리하는 로직을 구현합니다.
-      // ...
-    } else {
-      // 오류가 발생한 경우
-      print('Error: ${response.statusCode}');
-      // 오류 처리 로직을 구현합니다.
-      // ...
-    }
-  } catch (e) {
-    // 네트워크 요청 실패 등 예외가 발생한 경우
-    print('Exception occurred: $e');
-    // 예외 처리 로직을 구현합니다.
-    // ...
-  }
+  //   if (response.statusCode == 200) {
+  //     // 토큰을 성공적으로 받아온 경우
+  //     final token = response.data;
+  //     // 받아온 토큰을 앱에서 저장하거나 관리하는 로직을 구현합니다.
+  //     // ...
+  //   } else {
+  //     // 오류가 발생한 경우
+  //     print('Error: ${response.statusCode}');
+  //     // 오류 처리 로직을 구현합니다.
+  //     // ...
+  //   }
+  // } catch (e) {
+  //   // 네트워크 요청 실패 등 예외가 발생한 경우
+  //   print('Exception occurred: $e');
+  //   // 예외 처리 로직을 구현합니다.
+  //   // ...
+  // }
 }
